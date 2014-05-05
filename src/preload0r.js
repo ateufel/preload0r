885a0543fbf8bfa87e511bf0343dbed2ddfd3b30
Andreas Teufel <a.teufel@limesoda.at>
2014-05-05 11:29:11 +0200

    initial commit

---
+++ src/preload0r.js
@@ -0,0 +1,25 @@
+/*! preload0r - v1.0.0 - 2014-05-05
+ * https://github.com/luschn/preload0r
+ * Copyright (c) 2014 Andreas Teufel; Licensed MIT */
+/*
+ * preload0r
+ *
+ *
+ * Copyright (c) 2014 Andreas Teufel
+ * Licensed under the MIT license.
+ */
+
+(function (global) {
+    'use strict';
+
+    var images = [];
+
+    var Preload0r = function (paths) {
+        for (var i = 0, count = paths.length; i < count; i++) {
+            images[i] = new Image();
+            images[i].src = paths[i];
+        }
+    };
+
+    global.Preload0r = Preload0r;
+}(this));
