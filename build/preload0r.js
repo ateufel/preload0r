a1a789ec730173c62e6aac817b0f5aab6fde15fd
luschn <luschn@gmx.at>
2014-07-22 23:15:01 +0200

    add callbacks

--- build/preload0r.js
+++ build/preload0r.js
@@ -1,7 +1,7 @@
 /**
- * preload0r - Preload0r - Easy Image Preloader
- * @version v1.0.0
+ * preload0r - Easy Image Preloader
+ * @version v1.1.0
  * @link https://github.com/luschn/preload0r
  * @license MIT
  */
-!function(r){"use strict";var n=[],t=function(r){for(var t=0,e=r.length;e>t;t++)n[t]=new Image,n[t].src=r[t]};r.Preload0r=t}(this);
+!function(n){"use strict";var l,o,r=[],t=0,u=0,a=function(){u++,o&&o(u),u>=t&&l&&l()},c=function(n,u,c){l=u||null,o=c||null,t=n.length;for(var e=0;t>e;e++)r[e]=new Image,r[e].onload=a,r[e].src=n[e]};n.Preload0r=c}(this);
--- src/preload0r.js
+++ src/preload0r.js
@@ -1,5 +1,7 @@
-/*! preload0r - v1.0.0
- * https://github.com/luschn/preload0r
+/*!
+ * preload0r
+ *
+ *
  * Copyright (c) 2014 Andreas Teufel
  * Licensed under the MIT license.
  */
@@ -7,11 +9,29 @@
 (function (global) {
     'use strict';
 
-    var images = [];
+    var images = [],
+        count = 0,
+        loaded = 0,
+        callback,
+        callbackTick;
 
-    var Preload0r = function (paths) {
-        for (var i = 0, count = paths.length; i < count; i++) {
+    var onloadCallback = function () {
+        loaded++;
+        if (callbackTick) {
+            callbackTick(loaded);
+        }
+        if (loaded >= count && callback) {
+            callback();
+        }
+    };
+
+    var Preload0r = function (paths, cb, cbTick) {
+        callback = cb || null;
+        callbackTick = cbTick || null;
+        count = paths.length;
+        for (var i = 0; i < count; i++) {
             images[i] = new Image();
+            images[i].onload = onloadCallback;
             images[i].src = paths[i];
         }
     };
